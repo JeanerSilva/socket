366b7b71e4214a74330e1bcf5bc9c15b42109fe5
JeanerSilva <jeanerufmg@gmail.com>
2018-04-04 16:43:50 -0300

    First

---
+++ servidor-tarefas/src/br/com/alura/servidor/Main.java
@@ -0,0 +1,10 @@
+package br.com.alura.servidor;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		ServidorTarefas servidor = new ServidorTarefas();
+		servidor.rodar();
+	}
+
+}
